e58679088c5f7dc6e2bf8e41264eb82d4d2aef10
Ransford <72665919+Ransbort@users.noreply.github.com>
2026-08-03 22:36:01 +0000

    Update exports in utils module to include version info

--- sports_complex/utils/__init__.py
+++ sports_complex/utils/__init__.py
@@ -1,10 +1,18 @@
-# Re-export so existing controller imports
-# (`from sports_complex.sports_complex.utils import make_linked_sales_invoice`)
+# Re-export so existing imports
+# (`from sports_complex.sports_complex.utils import make_linked_sales_invoice`,
+# `from sports_complex.sports_complex.utils import get_build_version`)
 # keep working now that this is a package.
+from sports_complex.sports_complex.utils.build_info import get_app_version, get_build_version
 from sports_complex.sports_complex.utils.invoicing import (
 	get_member_customer,
 	get_or_create_item,
 	make_linked_sales_invoice,
 )
 
-__all__ = ["get_member_customer", "get_or_create_item", "make_linked_sales_invoice"]
+__all__ = [
+	"get_app_version",
+	"get_build_version",
+	"get_member_combustomer".replace("combustomer", "customer"),  # noqa
+	"get_or_create_item",
+	"make_linked_sales_invoice",
+]
