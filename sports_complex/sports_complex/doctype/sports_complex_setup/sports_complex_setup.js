d4e596d6e215e7f79299e6fe6d23631feaa7a83e
ransbort <ransbort@outlook.com>
2026-08-05 09:00:10 +0000

    doctype udpates

---
+++ sports_complex/sports_complex/doctype/sports_complex_setup/sports_complex_setup.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Ransbort and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Sports Complex Setup", {
+// 	refresh(frm) {
+
+// 	},
+// });
--- sports_complex/sports_complex/doctype/sports_settings/sports_settings.js
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2026, Sports Complex and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on("Sports Settings", {
-	// refresh(frm) {},
-});
