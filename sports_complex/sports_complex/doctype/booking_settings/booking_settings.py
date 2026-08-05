8b4df3bcd8c7476b45f6e180cc77051e44fcfa30
ransbort <ransbort@outlook.com>
2026-08-05 08:11:45 +0000

    deleted redundant settings doctypes

--- sports_complex/sports_complex/doctype/booking_settings/booking_settings.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2026, Your Company
-# License: MIT
-
-from frappe.model.document import Document
-
-
-class BookingSettings(Document):
-	pass
--- sports_complex/sports_complex/doctype/facility_settings/facility_settings.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2026, Your Company
-# License: MIT
-
-from frappe.model.document import Document
-
-
-class FacilitySettings(Document):
-	pass
